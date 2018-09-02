2e25c87ad4efa0795bc5cdd0e362a7e1cc9e85d7
Renan Porto <renan.porto1099@gmail.com>
2018-09-02 20:16:45 -0300

    Separando templates - adicionar arquivo e listar arquivo

--- main.py
+++ main.py
@@ -6,29 +6,35 @@ from werkzeug.utils import secure_filename
 app = Flask(__name__)
 app.run(debug=True)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def upload_file():
-    google_storage = Google_Storage('movti-interview')
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        file.filename = secure_filename(file.filename)
+    return render_template('upload_file.html')
 
-        public_url = google_storage.upload_file(
-            file.read(),
-            file.filename,
-            file.content_type
-        )
-        return render_template('upload_file.html', files=google_storage.list_blobs(), public_url=public_url)
-    return render_template('upload_file.html', files=google_storage.list_blobs())
+@app.route('/list_files', methods=['GET'])
+def list_files():
+    google_storage = Google_Storage('movti-interview')
+    return render_template('list_files.html', files=google_storage.list_blobs())
+
+@app.route('/upload_file_save', methods=['POST'])
+def upload_file_save():
+    google_storage = Google_Storage('movti-interview')
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    # if user does not select file, browser also
+    # submit a empty part without filename
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+    file.filename = secure_filename(file.filename)
+
+    return google_storage.upload_file(
+        file.read(),
+        file.filename,
+        file.content_type
+    )
 
 @app.route('/delete_file/<file_name>')
 def delete_file(file_name):
